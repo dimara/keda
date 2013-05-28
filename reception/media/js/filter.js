feee20376fe914139c2c7b64fe2b1ed610631ddd
Dimitris Aragiorgis <dimara@grnet.gr>
2013-05-28 05:29:30 +0300

    Paralia svg and png
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

---
+++ reception/media/js/filter.js
@@ -0,0 +1,15 @@
+$ = django.jQuery;
+
+$(document).ready(function(){
+
+  var $rows = $('#table tr');
+  $('#search').keyup(function() {
+      var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+      $rows.show().filter(function() {
+          var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+          return !~text.indexOf(val);
+      }).hide();
+  });
+
+})
