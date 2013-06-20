268b2d456cf9cd888075502a2f6f8bc5163951a1
Dimitris Aragiorgis <dimara@grnet.gr>
2013-06-21 01:17:15 +0300

    Remove unwatied files
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- reception/media/js/nested.js
+++
@@ -1,8 +0,0 @@
-$ = django.jQuery;
-
-$(document).ready(function(){
-
-  col = $('.nested-inline-row td').parent().prev().children().length;
-  $('.nested-inline-row td').first().attr("colspan", col);
-
-})
