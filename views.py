9eac793231982760d7a3a50ab10bff1ede774110
Dimitris Aragiorgis <dimara@grnet.gr>
2013-07-24 11:23:12 +0300

    Fix "too many sql variables" error
    First filter then prefetch_related.
    
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- views.py
+++ views.py
@@ -219,15 +219,15 @@ def reservations(request):
     period, start, end = get_start_end(request)
     rtype = request.GET.get("rtype", None)
     status = request.GET.get("status", None)
-    reservations = Reservation.objects.all().prefetch_related("owner",
-                                                              "appartment",
-                                                              "receipts")
+    reservations = Reservation.objects.all()
     if rtype:
         reservations = reservations.filter(res_type=rtype)
     if status:
         reservations = reservations.filter(status=status)
 
-    reservations = [r for r in reservations if r.inside(start, end)]
+    reservations = filter(lambda x: x.inside(start, end), reservations)
+    reservations = Reservation.objects.filter(id__in=[r.id for r in reservations])
+    reservations = reservations.prefetch_related("owner", "appartment", "receipts")
     reservations = sorted(reservations, key=lambda x: x.owner.surname)
     ctx = {
       "period": period,
