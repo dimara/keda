9889227a63259ba4370aaac0ae4a83603492457d
Dimitris Aragiorgis <dimara@grnet.gr>
2013-07-25 08:52:52 +0300

    Check for duplicates only for Person
    Because visitor inherits person form = PersonForm makes
    visitor fields not visible
    
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- reception/admin.py
+++ reception/admin.py
@@ -53,6 +53,16 @@ class NestedPersonAdmin(NestedModelAdmin):
     list_display = ('surname', 'name', )
     search_fields = ('surname', 'name', )
     ordering = ('surname', )
+    inlines = [
+        ContactInfoInline,
+        VehicleInline,
+        NestedReservationInline,
+        ]
+
+class NestedPersonAdmin2(NestedModelAdmin):
+    list_display = ('surname', 'name', )
+    search_fields = ('surname', 'name', )
+    ordering = ('surname', )
     form = PersonForm
     inlines = [
         ContactInfoInline,
@@ -142,7 +152,7 @@ class VehicleAdmin(admin.ModelAdmin):
     search_fields = ('plate', )
 
 admin.site.register(Period, PeriodAdmin)
-admin.site.register(Person, NestedPersonAdmin)
+admin.site.register(Person, NestedPersonAdmin2)
 admin.site.register(Rank, RankAdmin)
 admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(ContactInfo, ContactInfoAdmin)
