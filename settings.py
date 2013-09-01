65ad2c04712b74c0a8ef900e12cf31641ab3ee48
Dimitris Aragiorgis <dimara@grnet.gr>
2013-09-01 22:47:09 +0300

    Move all static under reception/static/reception
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- settings.py
+++ settings.py
@@ -59,13 +59,11 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ROOT_DIR + '/reception/media/'
 STATIC_ROOT = ROOT_DIR + '/staticfiles/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
