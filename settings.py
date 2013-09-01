f6ddb199c1cec91ee6eda8c105beb30d300980ae
Dimitris Aragiorgis <dimara@grnet.gr>
2013-09-02 00:03:02 +0300

    Fix login/logout urls
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- settings.py
+++ settings.py
@@ -96,6 +96,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ROOT_DIR + '/reception/templates/',
 )
 
 INSTALLED_APPS = (
