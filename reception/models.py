63ef1b6a6eef239256b0cb6a9ec8e3423e8db0bf
Dimitris Aragiorgis <dimara@grnet.gr>
2013-09-08 18:12:51 +0300

    Receipt fixes
    Add default datetime.
    Remove it from admin because adding/modifying a receipt
    takes for ages because of large number of reservations
    which requires lots of db queries.
    
    TODO: substitute reservations with check_in, check_out, status only.
    
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- reception/models.py
+++ reception/models.py
@@ -603,7 +603,7 @@ class InlineReservationForm(ReservationForm):
     pass
 
 class Receipt(models.Model):
-    date = models.DateTimeField("Date", null=True, blank=True)
+    date = models.DateTimeField("Date", default=datetime.datetime.now(), null=True, blank=True)
     no = models.CharField("No", max_length=10)
     reservation = models.ForeignKey(Reservation, related_name="receipts")
     euro = models.DecimalField("Euro", decimal_places=2, max_digits=10 )
