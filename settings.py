b50954ee58ef8d6373e4446891c855a67426fe29
Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>
2022-07-22 12:32:06 +0300

    Allow all hosts by default
    Signed-off-by: Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>

--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ DATABASES = {
 DATABASE_ENGINE = 'django.db.backends.sqlite3'
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
