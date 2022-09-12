5561e9a3aad7c2e260b9036db7be8dfb1d4dbf26
Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>
2022-09-12 11:15:10 +0300

    Use Google Maps API v3
    Follow official instructions and upgrade out Maps JavaScript API
    Application from V2 to V3 [1]. We also need to slightly change how
    we use markers [2,3].
    
    Use road map view instead of satellite since the latter is now pixelled
    in this area.
    
    [1] https://developers.google.com/maps/documentation/javascript/v2tov3#newkey
    [2] https://developers.google.com/maps/documentation/javascript/markers?hl=en#convertingtoicon
    [3] https://developers.google.com/maps/documentation/javascript/infowindows
    
    Signed-off-by: Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>

--- reception/static/reception/js/mapiconmaker.js
+++ reception/static/reception/js/mapiconmaker.js
@@ -1,8 +1,8 @@
 /**
  * @name MapIconMaker
- * @version 1.1
- * @author Pamela Fox
- * @copyright (c) 2008 Pamela Fox
+ * @version 2.0b
+ * @author Pamela Fox, Marc Climent
+ * @copyright (c) 2008 Pamela Fox, 2010 Marc Climent
  * @fileoverview This gives you static functions for creating dynamically
  *     sized and colored marker icons using the Charts API marker output.
  */
@@ -73,48 +73,54 @@ var MapIconMaker = {};
  * @return {GIcon}
  */
 MapIconMaker.createMarkerIcon = function (opts) {
-  var width = opts.width || 32;
-  var height = opts.height || 32;
-  var primaryColor = opts.primaryColor || "#ff0000";
-  var strokeColor = opts.strokeColor || "#000000";
-  var cornerColor = opts.cornerColor || "#ffffff";
-   
-  var baseUrl = "http://chart.apis.google.com/chart?cht=mm";
-  var iconUrl = baseUrl + "&chs=" + width + "x" + height + 
-      "&chco=" + cornerColor.replace("#", "") + "," + 
-      primaryColor.replace("#", "") + "," + 
-      strokeColor.replace("#", "") + "&ext=.png";
-  var icon = new GIcon(G_DEFAULT_ICON);
-  icon.image = iconUrl;
-  icon.iconSize = new GSize(width, height);
-  icon.shadowSize = new GSize(Math.floor(width * 1.6), height);
-  icon.iconAnchor = new GPoint(width / 2, height);
-  icon.infoWindowAnchor = new GPoint(width / 2, Math.floor(height / 12));
-  icon.printImage = iconUrl + "&chof=gif";
-  icon.mozPrintImage = iconUrl + "&chf=bg,s,ECECD8" + "&chof=gif";
-  iconUrl = baseUrl + "&chs=" + width + "x" + height + 
-      "&chco=" + cornerColor.replace("#", "") + "," + 
-      primaryColor.replace("#", "") + "," + 
-      strokeColor.replace("#", "");
-  icon.transparent = iconUrl + "&chf=a,s,ffffff11&ext=.png";
+    var width = opts.width || 32;
+    var height = opts.height || 32;
+    var primaryColor = opts.primaryColor || "#ff0000";
+    var strokeColor = opts.strokeColor || "#000000";
+    var cornerColor = opts.cornerColor || "#ffffff";
 
-  icon.imageMap = [
-    width / 2, height,
-    (7 / 16) * width, (5 / 8) * height,
-    (5 / 16) * width, (7 / 16) * height,
-    (7 / 32) * width, (5 / 16) * height,
-    (5 / 16) * width, (1 / 8) * height,
-    (1 / 2) * width, 0,
-    (11 / 16) * width, (1 / 8) * height,
-    (25 / 32) * width, (5 / 16) * height,
-    (11 / 16) * width, (7 / 16) * height,
-    (9 / 16) * width, (5 / 8) * height
-  ];
-  for (var i = 0; i < icon.imageMap.length; i++) {
-    icon.imageMap[i] = parseInt(icon.imageMap[i]);
-  }
+    var baseUrl = "http://chart.apis.google.com/chart?cht=mm";
+    var iconUrl = baseUrl + "&chs=" + width + "x" + height +
+        "&chco=" + cornerColor.replace("#", "") + "," +
+        primaryColor.replace("#", "") + "," +
+        strokeColor.replace("#", "") + "&ext=.png";
 
-  return icon;
+    var icon = new google.maps.MarkerImage();
+    var shadow = new google.maps.MarkerImage();
+
+    icon.url = iconUrl;
+    icon.anchor = new google.maps.Point(width / 2, height);
+    icon.size = new google.maps.Size(width, height);
+
+    shadow.url = 'http://maps.gstatic.com/mapfiles/shadow50.png';
+    shadow.anchor = new google.maps.Point(width / 2, height);
+    shadow.size = new google.maps.Size(Math.floor(width * 1.6), height);
+    shadow.scaledSize = new google.maps.Size(Math.floor(width * 1.6), height);
+
+    var coords = [
+      width / 2, height,
+      (7 / 16) * width, (5 / 8) * height,
+      (5 / 16) * width, (7 / 16) * height,
+      (7 / 32) * width, (5 / 16) * height,
+      (5 / 16) * width, (1 / 8) * height,
+      (1 / 2) * width, 0,
+      (11 / 16) * width, (1 / 8) * height,
+      (25 / 32) * width, (5 / 16) * height,
+      (11 / 16) * width, (7 / 16) * height,
+      (9 / 16) * width, (5 / 8) * height
+    ];
+    for (var i = 0; i < coords.length; i++) {
+        coords[i] = parseInt(coords[i]);
+    }
+
+    var iconShape = { type: "poly", coords: coords };
+
+    // return icon;
+    return {
+        icon: icon,
+        shadow: shadow,
+        shape: iconShape
+    };
 };
 
 
@@ -127,47 +133,52 @@ MapIconMaker.createMarkerIcon = function (opts) {
  * @return {GIcon}
  */
 MapIconMaker.createFlatIcon = function (opts) {
-  var width = opts.width || 32;
-  var height = opts.height || 32;
-  var primaryColor = opts.primaryColor || "#ff0000";
-  var shadowColor = opts.shadowColor || "#000000";
-  var label = MapIconMaker.escapeUserText_(opts.label) || "";
-  var labelColor = opts.labelColor || "#000000";
-  var labelSize = opts.labelSize || 0;
-  var shape = opts.shape ||  "circle";
-  var shapeCode = (shape === "circle") ? "it" : "itr";
+    var width = opts.width || 32;
+    var height = opts.height || 32;
+    var primaryColor = opts.primaryColor || "#ff0000";
+    var shadowColor = opts.shadowColor || "#000000";
+    var label = MapIconMaker.escapeUserText_(opts.label) || "";
+    var labelColor = opts.labelColor || "#000000";
+    var labelSize = opts.labelSize || 0;
+    var shape = opts.shape || "circle";
+    var shapeCode = (shape === "circle") ? "it" : "itr";
 
-  var baseUrl = "http://chart.apis.google.com/chart?cht=" + shapeCode;
-  var iconUrl = baseUrl + "&chs=" + width + "x" + height + 
-      "&chco=" + primaryColor.replace("#", "") + "," + 
-      shadowColor.replace("#", "") + "ff,ffffff01" +
-      "&chl=" + label + "&chx=" + labelColor.replace("#", "") + 
-      "," + labelSize;
-  var icon = new GIcon(G_DEFAULT_ICON);
-  icon.image = iconUrl + "&chf=bg,s,00000000" + "&ext=.png";
-  icon.iconSize = new GSize(width, height);
-  icon.shadowSize = new GSize(0, 0);
-  icon.iconAnchor = new GPoint(width / 2, height / 2);
-  icon.infoWindowAnchor = new GPoint(width / 2, height / 2);
-  icon.printImage = iconUrl + "&chof=gif";
-  icon.mozPrintImage = iconUrl + "&chf=bg,s,ECECD8" + "&chof=gif";
-  icon.transparent = iconUrl + "&chf=a,s,ffffff01&ext=.png";
-  icon.imageMap = []; 
-  if (shapeCode === "itr") {
-    icon.imageMap = [0, 0, width, 0, width, height, 0, height];
-  } else {
-    var polyNumSides = 8;
-    var polySideLength = 360 / polyNumSides;
-    var polyRadius = Math.min(width, height) / 2;
-    for (var a = 0; a < (polyNumSides + 1); a++) {
-      var aRad = polySideLength * a * (Math.PI / 180);
-      var pixelX = polyRadius + polyRadius * Math.cos(aRad);
-      var pixelY = polyRadius + polyRadius * Math.sin(aRad);
-      icon.imageMap.push(parseInt(pixelX), parseInt(pixelY));
+    var baseUrl = "http://chart.apis.google.com/chart?cht=" + shapeCode;
+    var iconUrl = baseUrl + "&chs=" + width + "x" + height +
+        "&chco=" + primaryColor.replace("#", "") + "," +
+        shadowColor.replace("#", "") + "ff,ffffff01" +
+        "&chl=" + label + "&chx=" + labelColor.replace("#", "") +
+        "," + labelSize;
+
+    var icon = new google.maps.MarkerImage();
+
+    icon.url = iconUrl + "&chf=bg,s,00000000" + "&ext=.png";
+    icon.size = new google.maps.Size(width, height);
+    icon.anchor = new google.maps.Point(width / 2, height / 2);
+
+    var iconShape;
+
+    if (shapeCode === "itr") {
+        iconShape = { type: "poly", coords: [0, 0, width, 0, width, height, 0, height] };
+    } else {
+        var polyNumSides = 8;
+        var polySideLength = 360 / polyNumSides;
+        var polyRadius = Math.min(width, height) / 2;
+        var coords = [];
+        for (var a = 0; a < (polyNumSides + 1) ; a++) {
+            var aRad = polySideLength * a * (Math.PI / 180);
+            var pixelX = polyRadius + polyRadius * Math.cos(aRad);
+            var pixelY = polyRadius + polyRadius * Math.sin(aRad);
+            coords.push(parseInt(pixelX), parseInt(pixelY));
+        }
+        iconShape = { type: "poly", coords: coords };
     }
-  }
 
-  return icon;
+    // return icon;
+    return {
+        icon: icon,
+        shape: iconShape
+    };
 };
 
 
@@ -180,31 +191,45 @@ MapIconMaker.createFlatIcon = function (opts) {
  * @return {GIcon}
  */
 MapIconMaker.createLabeledMarkerIcon = function (opts) {
-  var primaryColor = opts.primaryColor || "#DA7187";
-  var strokeColor = opts.strokeColor || "#000000";
-  var starPrimaryColor = opts.starPrimaryColor || "#FFFF00";
-  var starStrokeColor = opts.starStrokeColor || "#0000FF";
-  var label = MapIconMaker.escapeUserText_(opts.label) || "";
-  var labelColor = opts.labelColor || "#000000";
-  var addStar = opts.addStar || false;
-  
-  var pinProgram = (addStar) ? "pin_star" : "pin";
-  var baseUrl = "http://chart.apis.google.com/chart?cht=d&chdp=mapsapi&chl=";
-  var iconUrl = baseUrl + pinProgram + "'i\\" + "'[" + label + 
-      "'-2'f\\"  + "hv'a\\]" + "h\\]o\\" + 
-      primaryColor.replace("#", "")  + "'fC\\" + 
-      labelColor.replace("#", "")  + "'tC\\" + 
-      strokeColor.replace("#", "")  + "'eC\\";
-  if (addStar) {
-    iconUrl += starPrimaryColor.replace("#", "") + "'1C\\" + 
-        starStrokeColor.replace("#", "") + "'0C\\";
-  }
-  iconUrl += "Lauto'f\\";
+    var primaryColor = opts.primaryColor || "#DA7187";
+    var strokeColor = opts.strokeColor || "#000000";
+    var starPrimaryColor = opts.starPrimaryColor || "#FFFF00";
+    var starStrokeColor = opts.starStrokeColor || "#0000FF";
+    var label = MapIconMaker.escapeUserText_(opts.label) || "";
+    var labelColor = opts.labelColor || "#000000";
+    var addStar = opts.addStar || false;
 
-  var icon = new GIcon(G_DEFAULT_ICON);
-  icon.image = iconUrl + "&ext=.png";
-  icon.iconSize = (addStar) ? new GSize(23, 39) : new GSize(21, 34);
-  return icon;
+    var pinProgram = (addStar) ? "pin_star" : "pin";
+    var baseUrl = "http://chart.apis.google.com/chart?cht=d&chdp=mapsapi&chl=";
+    var iconUrl = baseUrl + pinProgram + "'i\\" + "'[" + label +
+        "'-2'f\\" + "hv'a\\]" + "h\\]o\\" +
+        primaryColor.replace("#", "") + "'fC\\" +
+        labelColor.replace("#", "") + "'tC\\" +
+        strokeColor.replace("#", "") + "'eC\\";
+    if (addStar) {
+        iconUrl += starPrimaryColor.replace("#", "") + "'1C\\" +
+            starStrokeColor.replace("#", "") + "'0C\\";
+    }
+    iconUrl += "Lauto'f\\";
+
+    var icon = new google.maps.MarkerImage();
+    var shadow = new google.maps.MarkerImage();
+
+    var width = (addStar) ? 23 : 21;
+    var height = (addStar) ? 39 : 34;
+
+    icon.url = iconUrl + "&ext=.png";
+    icon.size = new google.maps.Size(width, height);
+
+    shadow.url = 'http://maps.gstatic.com/mapfiles/shadow50.png';
+    shadow.anchor = new google.maps.Point(width / 2, height);
+    shadow.size = new google.maps.Size(Math.floor(width * 1.6), height);
+    shadow.scaledSize = new google.maps.Size(Math.floor(width * 1.6), height);
+
+    return {
+        icon: icon,
+        shadow: shadow
+    };
 };
 
 
@@ -214,14 +239,13 @@ MapIconMaker.createLabeledMarkerIcon = function (opts) {
  * @private
  */
 MapIconMaker.escapeUserText_ = function (text) {
-  if (text === undefined) {
-    return null;
-  }
-  text = text.replace(/@/, "@@");
-  text = text.replace(/\\/, "@\\");
-  text = text.replace(/'/, "@'");
-  text = text.replace(/\[/, "@[");
-  text = text.replace(/\]/, "@]");
-  return encodeURIComponent(text);
+    if (text === undefined) {
+        return null;
+    }
+    text = text.replace(/@/, "@@");
+    text = text.replace(/\\/, "@\\");
+    text = text.replace(/'/, "@'");
+    text = text.replace(/\[/, "@[");
+    text = text.replace(/\]/, "@]");
+    return encodeURIComponent(text);
 };
-
